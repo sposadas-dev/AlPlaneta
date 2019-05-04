3f3c3d9667872f8e4844cab3f651c87ede5433fc
Sebastian Posadas <posadas.sca@gmail.com>
2019-05-04 15:47:15 -0300

    Querys de PagoDAOSQL

--- src/main/java/persistencia/dao/mysql/PagoDAOSQL.java
+++ src/main/java/persistencia/dao/mysql/PagoDAOSQL.java
@@ -1,10 +1,13 @@
 package persistencia.dao.mysql;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import dto.PagoDTO;
+import dto.PasajeroDTO;
 import persistencia.conexion.Conexion;
 import persistencia.dao.interfaz.PagoDAO;
 
@@ -14,6 +17,7 @@ public class PagoDAOSQL implements PagoDAO {
 	private static final String readall = "SELECT * FROM pago";
 	private static final String delete = "DELETE FROM pago WHERE idPago=?";
 	private static final String update = "UPDATE pago SET fechaPago=?, monto=? WHERE idPago=?;";
+	private static final String browse = "SELECT * FROM pago WHERE idPago=?";
 	
 	@Override
 	public boolean insert(PagoDTO pagoInsert) {
@@ -37,25 +41,85 @@ public class PagoDAOSQL implements PagoDAO {
 
 	@Override
 	public boolean delete(PagoDTO pagoDelete) {
-		// TODO Auto-generated method stub
+		PreparedStatement statement;
+		int chequeoUpdate = 0;
+		Conexion conexion = Conexion.getConexion();
+		try {
+			statement = conexion.getSQLConexion().prepareStatement(delete);
+			statement.setString(1, Integer.toString(pagoDelete.getIdPago()));
+			chequeoUpdate = statement.executeUpdate();
+			if (chequeoUpdate > 0)
+				return true;
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public List<PagoDTO> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement statement;
+		ResultSet resultSet;
+		Conexion conexion = Conexion.getConexion();
+		ArrayList<PagoDTO> pagos = new ArrayList<PagoDTO>();
+		try {
+			statement = conexion.getSQLConexion().prepareStatement(readall);
+			resultSet = statement.executeQuery();
+
+			while (resultSet.next()) {
+				pagos.add(new PagoDTO(resultSet.getInt("idPago"),
+								      resultSet.getDate("fechaPago"),
+									  resultSet.getBigDecimal("monto")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pagos;
 	}
 
 	@Override
 	public boolean update(PagoDTO pagoUpdate) {
-		// TODO Auto-generated method stub
+		PreparedStatement statement;
+		int chequeoUpdate = 0;
+		Conexion conexion = Conexion.getConexion();
+		try {
+			statement = conexion.getSQLConexion().prepareStatement(update);
+
+			statement.setDate(1, pagoUpdate.getFechaPago());
+			statement.setBigDecimal(2, pagoUpdate.getMonto());
+			statement.setInt(3, pagoUpdate.getIdPago());
+			chequeoUpdate = statement.executeUpdate();
+			if (chequeoUpdate > 0)
+				return true;
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
 	public PagoDTO browse(int idPago) {
-		// TODO Auto-generated method stub
+		PreparedStatement statement;
+		ResultSet resultSet;
+		Conexion conexion = Conexion.getConexion();
+		PagoDTO pago;
+		try {
+			statement = conexion.getSQLConexion().prepareStatement(browse);
+			
+			statement.setInt(1, idPago);
+			resultSet = statement.executeQuery();
+			if (resultSet.next()){
+				pago = new PagoDTO(resultSet.getInt("idPasajero"),
+										   resultSet.getDate("fechaPago"),
+										   resultSet.getBigDecimal("monto")
+										   );
+				return pago;
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
