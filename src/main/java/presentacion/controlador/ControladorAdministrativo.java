482aaf0f9cf5ca5681fba81ce707adf00fcef40c
Nicolas Avila <av.m.nico@gmail.com>
2019-06-18 13:21:28 -0300

    esta comentado la resta de dos dias al dia de la fecha para que se pueda enviar el voucher a los pasajes que salen el dia de hoy

--- src/main/java/presentacion/controlador/ControladorAdministrativo.java
+++ src/main/java/presentacion/controlador/ControladorAdministrativo.java
@@ -1106,9 +1106,11 @@ public class ControladorAdministrativo implements ActionListener {
 
 		Calendar calendar = Calendar.getInstance(); //obtiene la fecha de hoy
 		SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+// TODO: CAMBIAR LA FECHA..		
 //		calendar.add(Calendar.DATE, -2); //el -2 indica que se le restaran 2 dias
 		
 		for(PasajeDTO p : modeloPasaje.obtenerPasajes()) {
+			
 			String fechaLimite = format.format(calendar.getTime());
 			String fechaDelViaje = mapper.parseToStringJavaUtil(p.getViaje().getFechaSalida());
 			
